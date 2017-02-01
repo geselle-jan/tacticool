dcc058980e3d118a757dcb70974c9ed0d46a620b
Jan Geselle <geselle.jan@googlemail.com>
2017-02-01 18:27:50 +0100

    added text to buttons

--- Button.py
+++ Button.py
@@ -1,4 +1,5 @@
 from Rectangle import Rectangle
+import pygame
 
 class Button():
     def __init__(self, game, position, label, function):
@@ -6,13 +7,18 @@ class Button():
         self.init(position, label, function)
 
     def init(self, position, label, function):
-        self.background = Rectangle(self.game, position, [128,16], [32,32,32])
+        self.position = position
+        self.background = Rectangle(self.game, position, [48,16], [32,32,32])
+        pygame.font.init()
+        self.font = pygame.font.Font('assets/fonts/Munro.ttf', 14)
+        self.textSurface = self.font.render(label, False, (255, 255, 255))
 
     def update(self, deltaTime, events):
         pass
 
     def draw(self, screen):
         self.background.draw(screen)
+        screen.blit(self.textSurface, (self.position[0] + 2, self.position[1] + 1))
 
     def destroy(self):
         pass
